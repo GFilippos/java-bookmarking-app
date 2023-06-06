eb2064c7b5efe06390e3ee9165bf8b0215f903d9
GFilippos <gomelisfil@gmail.com>
2023-06-06 22:04:34 +0300

    completed concrete classes and first commit to remote repo

---
+++ src/com/gfdevo/bookmarkingapp/constants/BookGenre.java
@@ -0,0 +1,11 @@
+package com.gfdevo.bookmarkingapp.constants;
+
+public class BookGenre {
+    private BookGenre() {}
+
+    public static final String ART = "Art";
+    public static final String HISTORY = "History";
+    public static final String ROMANCE = "Romance";
+    public static final String MYSTERY = "Mystery";
+    public static final String MOTIVATIONAL = "Motivational";
+}
---
+++ src/com/gfdevo/bookmarkingapp/constants/MovieGenre.java
@@ -0,0 +1,11 @@
+package com.gfdevo.bookmarkingapp.constants;
+
+public class MovieGenre {
+    private MovieGenre() {}
+
+    public static final String CLASSICS = "Classics";
+    public static final String DRAMA = "Drama";
+    public static final String COMEDY = "Comedy";
+    public static final String HORROR = "Horror";
+    public static final String CHILDREN = "Children";
+}
